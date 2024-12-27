e8eafcf60e58f838182a70aece438eaa6f4e8f00
Pete Cornish <outofcoffee@gmail.com>
2024-12-27 20:14:40 +0000

    refactor: moves config load to config package.

--- cmd/imposter/main.go
+++ cmd/imposter/main.go
@@ -3,18 +3,16 @@ package main
 import (
 	"fmt"
 	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/gatehill/imposter-go/internal/config"
-	"github.com/gatehill/imposter-go/internal/parser"
+
 	"github.com/gatehill/imposter-go/internal/server"
 )
 
 func main() {
 	fmt.Println("Starting Imposter-Go...")
 
-	imposterConfig := config.LoadConfig()
+	imposterConfig := config.LoadImposterConfig()
 
 	if len(os.Args) < 2 {
 		panic("Config directory path must be provided as the first argument")
@@ -25,43 +23,7 @@ func main() {
 		panic("Specified path is not a valid directory")
 	}
 
-	var configs []config.Config
-
-	scanRecursive := (os.Getenv("IMPOSTER_CONFIG_SCAN_RECURSIVE") == "true")
-
-	err := filepath.Walk(configDir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		// Skip subdirectories if not scanning recursively
-		if info.IsDir() && info.Name() != filepath.Base(configDir) && !scanRecursive {
-			return filepath.SkipDir
-		}
-
-		if !info.IsDir() && (strings.HasSuffix(info.Name(), "-config.json") || strings.HasSuffix(info.Name(), "-config.yaml") || strings.HasSuffix(info.Name(), "-config.yml")) {
-			fmt.Printf("Loading config file: %s\n", path)
-			fileConfig, err := parser.ParseConfig(path)
-			if err != nil {
-				return err
-			}
-			// Prefix 'File' properties if in a subdirectory
-			baseDir := filepath.Dir(path)
-			relDir, err := filepath.Rel(configDir, baseDir)
-			if err != nil {
-				return err
-			}
-			for i := range fileConfig.Resources {
-				if fileConfig.Resources[i].Response.File != "" && relDir != "." {
-					fileConfig.Resources[i].Response.File = filepath.Join(relDir, fileConfig.Resources[i].Response.File)
-				}
-			}
-			configs = append(configs, *fileConfig)
-		}
-		return nil
-	})
-	if err != nil {
-		panic(err)
-	}
+	configs := config.LoadConfig(configDir)
 
 	// Optional: check that at least one config is rest
 	for _, cfg := range configs {
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,13 @@
 package config
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
+
+	"gopkg.in/yaml.v3"
 )
 
 // Response represents an HTTP response
@@ -31,8 +37,8 @@ type ImposterConfig struct {
 	ServerPort string
 }
 
-// LoadConfig loads configurations from environment variables
-func LoadConfig() *ImposterConfig {
+// LoadImposterConfig loads configurations from environment variables
+func LoadImposterConfig() *ImposterConfig {
 	port := os.Getenv("IMPOSTER_PORT")
 	if port == "" {
 		port = "8080" // Default port
@@ -41,4 +47,61 @@ func LoadConfig() *ImposterConfig {
 	return &ImposterConfig{
 		ServerPort: port,
 	}
+}
+
+// LoadConfig loads all config files in the specified directory
+func LoadConfig(configDir string) []Config {
+	var configs []Config
+
+	scanRecursive := (os.Getenv("IMPOSTER_CONFIG_SCAN_RECURSIVE") == "true")
+
+	err := filepath.Walk(configDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		// Skip subdirectories if not scanning recursively
+		if info.IsDir() && info.Name() != filepath.Base(configDir) && !scanRecursive {
+			return filepath.SkipDir
+		}
+
+		if !info.IsDir() && (strings.HasSuffix(info.Name(), "-config.json") || strings.HasSuffix(info.Name(), "-config.yaml") || strings.HasSuffix(info.Name(), "-config.yml")) {
+			fmt.Printf("Loading config file: %s\n", path)
+			fileConfig, err := parseConfig(path)
+			if err != nil {
+				return err
+			}
+			// Prefix 'File' properties if in a subdirectory
+			baseDir := filepath.Dir(path)
+			relDir, err := filepath.Rel(configDir, baseDir)
+			if err != nil {
+				return err
+			}
+			for i := range fileConfig.Resources {
+				if fileConfig.Resources[i].Response.File != "" && relDir != "." {
+					fileConfig.Resources[i].Response.File = filepath.Join(relDir, fileConfig.Resources[i].Response.File)
+				}
+			}
+			configs = append(configs, *fileConfig)
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	return configs
+}
+
+// parseConfig loads and parses a YAML configuration file
+func parseConfig(path string) (*Config, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %w", err)
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal YAML: %w", err)
+	}
+
+	return &cfg, nil
 }
