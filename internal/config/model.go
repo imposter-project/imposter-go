de69ff3ea68a8ccd68150c129849d879ce41ac08
Alex Saunders <acsauk@gmail.com>
2025-08-21 16:16:01 +0100

    allow external $refs

--- internal/config/model.go
+++ internal/config/model.go
@@ -404,6 +404,7 @@ type Config struct {
 
 	// OpenAPI-specific fields
 	SpecFile        string            `yaml:"specFile,omitempty"`
+	ExternalBaseURL string            `yaml:"externalBaseUrl,omitempty"`
 	StripServerPath bool              `yaml:"stripServerPath,omitempty"`
 	Validation      *ValidationConfig `yaml:"validation,omitempty"`
 
